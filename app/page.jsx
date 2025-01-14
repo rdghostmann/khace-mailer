424d077eee56d6ce47e986c227b673685e0c519a
unknown <randalchukzwilson@gmail.com>
2025-01-14 15:59:36 +0100

    Updated  HTML content commit2

--- app/page.jsx
+++ app/page.jsx
@@ -9,14 +9,14 @@ export default function Home() {
   return (
     <main className="flex flex-col items-center justify-between p-24">
       <h1 className="text-sm">Welcome to noreply-Deskcheck GC </h1>
-      <Link href="/testpage" className="flex space-x-2 items-center justify-center rounded-md bg-slate-700 text-white p-3"><FaRegEdit  />
-      <span>Go to KhaceMailer</span></Link>
-    
-    <div>
-    <Link target="_blank" href="https://khacemailer.vercel.app/canadacongratstemplate.html"> HTML Canada Congrats Template:<LuCornerDownRight /></Link>
-    </div>
+      <Link href="/testpage" className="flex space-x-2 items-center justify-center rounded-md bg-slate-700 text-white p-3"><FaRegEdit />
+        <span>Go to KhaceMailer</span></Link>
 
-  
+      <div>
+        <Link target="_blank" href="https://khacemailer.vercel.app/canadacongratstemplate.txt"> HTML Canada Congrats Template:<LuCornerDownRight /></Link>
+      </div>
+
+
     </main>
   );
 }
