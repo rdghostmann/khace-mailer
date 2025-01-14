d2c314f00f1a8ad22cb284f9366f580b89435c98
unknown <randalchukzwilson@gmail.com>
2025-01-14 16:04:12 +0100

    Updated  HTML content commit3

--- app/page.jsx
+++ app/page.jsx
@@ -12,8 +12,8 @@ export default function Home() {
       <Link href="/testpage" className="flex space-x-2 items-center justify-center rounded-md bg-slate-700 text-white p-3"><FaRegEdit />
         <span>Go to KhaceMailer</span></Link>
 
-      <div>
-        <Link target="_blank" href="https://khacemailer.vercel.app/canadacongratstemplate.txt"> HTML Canada Congrats Template:<LuCornerDownRight /></Link>
+      <div className="w-4/5 mx-auto text-center mt-4">
+        <Link className="flex items-center justify-center space-x-1" target="_blank" href="https://khacemailer.vercel.app/canadacongratstemplate.txt"> <span>HTML Canada Congrats Template:</span> <LuCornerDownRight /></Link>
       </div>
 
 
